5011fd2ab2060f842a81950782cbcf80226fc2c2
Paola <paola.obreguillermo@yahoo.com>
2019-06-17 23:40:22 -0500

    database working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,18 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Dog.findAll({}).then(function(dbExamples) {
+      console.log (dbExamples)
       res.render("index", {
-        msg: "Welcome!",
+        msg: "Welcome to Dog Home!",
         examples: dbExamples
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
+  app.get("/dog/:id", function(req, res) {
+    db.Dog.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("dog", {
         example: dbExample
       });
     });
